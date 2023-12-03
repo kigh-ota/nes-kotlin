9988a8f910b415d4e7cd9726a70694e04ef295c4
Kaiichiro <kigh.ota@gmail.com>
2023-12-03 21:21:50 +0900

    load and run

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,23 +2,67 @@ fun main(args: Array<String>) {
     println("Hello")
 }
 
-typealias Int8 = Int
-typealias Int16 = Int
+typealias u8 = Int
+typealias u16 = Int
 
 class CPU {
-    private var pc: Int16 = 0
-    var a: Int8 = 0
-    var x: Int8 = 0
-    var status: Int8 = 0
+    companion object {
+        private val RESET_LOCATION: u16 = 0xFFFC
+    }
 
-    fun interpret(program: List<Int8>) {
-        pc = 0
+    private var pc: u16 = 0
+    var a: u8 = 0
+    var x: u8 = 0
+    var status: u8 = 0
+    private val memory = MutableList(0xFFFF) { 0 }
+
+    private fun readMemoryU8(addr: u16) = memory[addr]
+    private fun writeMemoryU8(addr: u16, data: u8) {
+        memory[addr] = data
+    }
+
+    // little-endian
+    private fun readMemoryU16(pos: u16): u16 {
+        val lo = readMemoryU8(pos)
+        val hi = readMemoryU8(pos + 1)
+        return hi.shl(8).or(lo)
+    }
+
+    private fun writeMemoryU16(pos: u16, data: u16) {
+        val hi = data.shr(8)
+        val lo = data.and(0xff)
+        writeMemoryU8(pos, lo)
+        writeMemoryU8(pos + 1, hi)
+    }
+
+    fun reset() {
+        a = 0
+        x = 0
+        status = 0
+
+        pc = readMemoryU16(RESET_LOCATION)
+    }
+
+    fun loadAndRun(program: List<u8>) {
+        load(program)
+        reset()
+        run()
+    }
+
+    fun load(program: List<u8>) {
+        program.forEachIndexed { i, d ->
+            memory[0x8000 + i] = d
+        }
+        writeMemoryU16(RESET_LOCATION, 0x8000)
+    }
+
+    fun run() {
         repeat(10000) {
-            val opcode = program[pc]
+            val opcode = readMemoryU8(pc)
             pc++
             when (opcode) {
                 0xA9 -> {   // LDA
-                    val param = program[pc]
+                    val param = readMemoryU8(pc)
                     pc++
                     lda(param)
                 }
@@ -28,9 +72,10 @@ class CPU {
                 else -> error("Not implemented")
             }
         }
+
     }
 
-    private fun lda(value: Int8) {
+    private fun lda(value: u8) {
         a = value
         updateZeroAndNegativeFlags(a)
     }
@@ -45,16 +90,18 @@ class CPU {
         updateZeroAndNegativeFlags(x)
     }
 
-    private fun updateZeroAndNegativeFlags(result: Int8) {
+    private fun updateZeroAndNegativeFlags(result: u8) {
+        // zero flag
         status = if (result == 0) {
             status.or(0b0000_0010)
         } else {
             status.and(0b1111_1101)
         }
+        // negative flag
         status = if (result.and(0b1000_0000) != 0) {
             status.or(0b1000_0000)
         } else {
-            status.and(0x0111_1111)
+            status.and(0b0111_1111)
         }
     }
 }
--- src/test/kotlin/CPUTest.kt
+++ src/test/kotlin/CPUTest.kt
@@ -5,7 +5,7 @@ class CPUTest {
     @Test
     fun testLdaImmediateLoadData() {
         val cpu = CPU()
-        cpu.interpret(listOf(0xa9, 0x05, 0x00))
+        cpu.loadAndRun(listOf(0xa9, 0x05, 0x00))
         assertThat(cpu.a).isEqualTo(0x05)
         assertThat(cpu.status.and(0b0000_0010)).isEqualTo(0)
         assertThat(cpu.status.and(0b1000_0000)).isEqualTo(0)
@@ -14,30 +14,14 @@ class CPUTest {
     @Test
     fun testLdaZeroFlag() {
         val cpu = CPU()
-        cpu.interpret(listOf(0xa9, 0x00, 0x00))
+        cpu.loadAndRun(listOf(0xa9, 0x00, 0x00))
         assertThat(cpu.status.and(0b0000_0010)).isEqualTo(0b10)
-    }
-
-    @Test
-    fun testTaxMoveAToX() {
-        val cpu = CPU()
-        cpu.a = 10
-        cpu.interpret(listOf(0xaa, 0x00))
-        assertThat(cpu.x).isEqualTo(10)
     }
 
     @Test
     fun testFiveOpsWorkingTogether() {
         val cpu = CPU()
-        cpu.interpret(listOf(0xa9, 0xc0, 0xaa, 0xe8, 0x00))
+        cpu.loadAndRun(listOf(0xa9, 0xc0, 0xaa, 0xe8, 0x00))
         assertThat(cpu.x).isEqualTo(0xc1)
-    }
-
-    @Test
-    fun testInxOverflow() {
-        val cpu = CPU()
-        cpu.x = 0xff
-        cpu.interpret(listOf(0xe8, 0xe8, 0x00))
-        assertThat(cpu.x).isEqualTo(1)
     }
 }
