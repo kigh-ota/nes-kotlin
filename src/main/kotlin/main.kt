b03d18a709d6d4711cf6f7182ad9928151f32700
Kaiichiro <kigh.ota@gmail.com>
2023-12-03 20:44:33 +0900

    implement TAX & INX

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,10 +7,9 @@ typealias Int16 = Int
 
 class CPU {
     private var pc: Int16 = 0
-    private var a: Int8 = 0
-    fun getA() = a
-    private var status: Int8 = 0
-    fun getStatus() = status
+    var a: Int8 = 0
+    var x: Int8 = 0
+    var status: Int8 = 0
 
     fun interpret(program: List<Int8>) {
         pc = 0
@@ -18,31 +17,44 @@ class CPU {
             val opcode = program[pc]
             pc++
             when (opcode) {
-                0xA9 -> {
+                0xA9 -> {   // LDA
                     val param = program[pc]
                     pc++
-                    a = param
-
-                    status = if (a == 0) {
-                        status.or(0b0000_0010)
-                    } else {
-                        status.and(0b1111_1101)
-                    }
-
-                    status = if (a.and(0b1000_0000) != 0) {
-                        status.or(0b1000_0000)
-                    } else {
-                        status.and(0b0111_1111)
-                    }
+                    lda(param)
                 }
-                0xAA -> {
-
-                }
-                0x00 -> {
-                    return
-                }
+                0xAA -> tax()
+                0xE8 -> inx()
+                0x00 -> return // BRK
                 else -> error("Not implemented")
             }
+        }
+    }
+
+    private fun lda(value: Int8) {
+        a = value
+        updateZeroAndNegativeFlags(a)
+    }
+
+    private fun tax() {
+        x = a
+        updateZeroAndNegativeFlags(x)
+    }
+
+    private fun inx() {
+        x = (x + 1) % 256
+        updateZeroAndNegativeFlags(x)
+    }
+
+    private fun updateZeroAndNegativeFlags(result: Int8) {
+        status = if (result == 0) {
+            status.or(0b0000_0010)
+        } else {
+            status.and(0b1111_1101)
+        }
+        status = if (result.and(0b1000_0000) != 0) {
+            status.or(0b1000_0000)
+        } else {
+            status.and(0x0111_1111)
         }
     }
 }
--- src/test/kotlin/CPUTest.kt
+++ src/test/kotlin/CPUTest.kt
@@ -6,15 +6,38 @@ class CPUTest {
     fun testLdaImmediateLoadData() {
         val cpu = CPU()
         cpu.interpret(listOf(0xa9, 0x05, 0x00))
-        assertThat(cpu.getA()).isEqualTo(0x05)
-        assertThat(cpu.getStatus().and(0b0000_0010)).isEqualTo(0)
-        assertThat(cpu.getStatus().and(0b1000_0000)).isEqualTo(0)
+        assertThat(cpu.a).isEqualTo(0x05)
+        assertThat(cpu.status.and(0b0000_0010)).isEqualTo(0)
+        assertThat(cpu.status.and(0b1000_0000)).isEqualTo(0)
     }
 
     @Test
     fun testLdaZeroFlag() {
         val cpu = CPU()
         cpu.interpret(listOf(0xa9, 0x00, 0x00))
-        assertThat(cpu.getStatus().and(0b0000_0010)).isEqualTo(0b10)
+        assertThat(cpu.status.and(0b0000_0010)).isEqualTo(0b10)
+    }
+
+    @Test
+    fun testTaxMoveAToX() {
+        val cpu = CPU()
+        cpu.a = 10
+        cpu.interpret(listOf(0xaa, 0x00))
+        assertThat(cpu.x).isEqualTo(10)
+    }
+
+    @Test
+    fun testFiveOpsWorkingTogether() {
+        val cpu = CPU()
+        cpu.interpret(listOf(0xa9, 0xc0, 0xaa, 0xe8, 0x00))
+        assertThat(cpu.x).isEqualTo(0xc1)
+    }
+
+    @Test
+    fun testInxOverflow() {
+        val cpu = CPU()
+        cpu.x = 0xff
+        cpu.interpret(listOf(0xe8, 0xe8, 0x00))
+        assertThat(cpu.x).isEqualTo(1)
     }
 }
